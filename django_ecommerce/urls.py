6b875b15a818d5ff90be72048c97f3703650d883
Michael Browne <arcticfeather@hotmail.com>
2015-01-09 10:37:48 +0900

    Basic template complete

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_ecommerce.views.home', name='home'),
     # url(r'^django_ecommerce/', include('django_ecommerce.foo.urls')),
+    url(r'^$', 'main.views.index', name='home'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
