7c9e07ffccca4459ded2d834c171111bd962deb7
Michael Browne <arcticfeather@hotmail.com>
2015-01-09 21:50:51 +0900

    Registration and payment added - Book 2 complete

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, "static"),
+    os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     # my apps
     'main',
     'contact',
+    'payments',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -164,3 +165,7 @@ LOGGING = {
         },
     }
 }
+
+# Stripe API Keys
+STRIPE_SECRET = 'sk_test_qU3R1w76mpdEfXCGBjFAAOjL'
+STRIPE_PUBLISHABLE = 'pk_test_BfmZg52Awp480sCtIH6dOROu'
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from payments import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,6 +12,11 @@ urlpatterns = patterns('',
     url(r'^$', 'main.views.index', name='home'),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^contact/', 'contact.views.contact', name='contact'),
+    # user registration/authentication
+    url(r'^sign_in$', views.sign_in, name='sign_in'),
+    url(r'^sign_out$', views.sign_out, name='sign_out'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^edit$', views.edit, name='edit'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
